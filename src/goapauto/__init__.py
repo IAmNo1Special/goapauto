f336325109e058b26997b4ba4ee4798419ee6522
IAmNo1Special <ivmno1special@gmail.com>
2026-08-01 08:21:36 -0400

    chore: bump version to 0.2.5

--- src/goapauto/__init__.py
+++ src/goapauto/__init__.py
@@ -61,7 +61,7 @@ from goapauto.models.sensors import Sensor, SensorManager
 from goapauto.models.worldstate import WorldState
 from goapauto.utils.visualizer import SearchTreeVisualizer
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __all__ = [
     "Planner",
     "Goal",
